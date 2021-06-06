473ef1dbecf6fb8be716998f8cfefe44a8f2f629
konawasabi <webmaster@konawasabi.riceball.jp>
2021-06-06 21:29:58 +0900

    アンダーバーを読み込まないバグ解消

--- loadheader.py
+++ loadheader.py
@@ -20,7 +20,7 @@ def loadheader(path,HEAD_STR,HEAD_VER):
         raise
 
     header_version = float(re.findall(r'\d+.\d+',header)[0]) # 入力文字列からバージョン番号'xxxx.xxxxx'に最初に一致する部分を取り出す
-    header_encoding = 'utf-8' if re.findall(r':[a-zA-z][a-zA-Z0-9\-_]+',header) == [] else re.findall(r':[a-zA-z][a-zA-Z0-9\-]+',header)[0][1:] # エンコーディング指定':hoge1-huga2'と最初に一致する部分を取り出し、文頭コロンを除外。見つからない時は'utf-8'
+    header_encoding = 'utf-8' if re.findall(r':[a-zA-z][a-zA-Z0-9\-_]+',header) == [] else re.findall(r':[a-zA-z][a-zA-Z0-9\-_]+',header)[0][1:] # エンコーディング指定':hoge1-huga2'と最初に一致する部分を取り出し、文頭コロンを除外。見つからない時は'utf-8'
     if(header_version < HEAD_VER):
         raise
 
