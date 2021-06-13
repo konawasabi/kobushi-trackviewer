b48a19daf2bb910bf4a6f8e2f6b849d81333b6ab
konawasabi <webmaster@konawasabi.riceball.jp>
2021-06-13 09:16:22 +0900

    エンコード指定がshift_jis or sjisならCP932として読み込む

--- loadheader.py
+++ loadheader.py
@@ -24,6 +24,9 @@ def loadheader(path,HEAD_STR,HEAD_VER):
     header_encoding = 'utf-8' if re.findall(r':[a-zA-z][a-zA-Z0-9\-_]+',header) == [] else re.findall(r':[a-zA-z][a-zA-Z0-9\-_]+',header)[0][1:] # エンコーディング指定':hoge1-huga2'と最初に一致する部分を取り出し、文頭コロンを除外。見つからない時は'utf-8'
     if(header_version < HEAD_VER):
         raise
+    
+    if(header_encoding.casefold() == 'shift_jis'.casefold() or header_encoding.casefold() == 'sjis'.casefold()):
+        header_encoding = 'CP932'
 
     return input, rootpath, header_encoding
     #return open(input,'r',encoding=header_encoding), str(input), rootpath
