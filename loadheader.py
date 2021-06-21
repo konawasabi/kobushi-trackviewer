395e84686d1e2a1d8755ee14e6f1a3b0f36eac5b
konawasabi <webmaster@konawasabi.riceball.jp>
2021-06-21 13:27:24 +0900

    ファイルオープンに関する具体的な例外発出

--- loadheader.py
+++ loadheader.py
@@ -15,15 +15,15 @@ def loadheader(path,HEAD_STR,HEAD_VER):
         header = f.readline().decode('utf-8') #一行目をutf-8でデコード。
         f.close()
     except Exception as e:
-        raise
+        raise OSError()
 
     if(HEAD_STR.casefold() not in header.casefold()):
-        raise
+        raise RuntimeError(str(path) + ' is not ' + HEAD_STR)
 
     header_version = float(re.findall(r'\d+.\d+',header)[0]) # 入力文字列からバージョン番号'xxxx.xxxxx'に最初に一致する部分を取り出す
     header_encoding = 'utf-8' if re.findall(r':[a-zA-z][a-zA-Z0-9\-_]+',header) == [] else re.findall(r':[a-zA-z][a-zA-Z0-9\-_]+',header)[0][1:] # エンコーディング指定':hoge1-huga2'と最初に一致する部分を取り出し、文頭コロンを除外。見つからない時は'utf-8'
     if(header_version < HEAD_VER):
-        raise
+        raise RuntimeError(str(path) + ' is under Ver.' +str(HEAD_VER))
     
     if(header_encoding.casefold() == 'shift_jis'.casefold() or header_encoding.casefold() == 'sjis'.casefold()):
         header_encoding = 'CP932'
