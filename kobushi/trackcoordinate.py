5edafd873c7c9790bd9ba484a4d681bc59ae8cd9
konawasabi <webmaster@konawasabi.riceball.jp>
2022-04-22 00:32:47 +0900

    curve classをharfsin transitionに対応

--- kobushi/trackcoordinate.py
+++ kobushi/trackcoordinate.py
@@ -113,7 +113,7 @@ class curve():
         r1 = np.inf if r1==0 else r1
         r2 = np.inf if r2==0 else r2
 
-        if True: # 直線逓減の場合
+        if func == 'line': # 直線逓減の場合
             L0 = L*(1-(1/(1-(r2)/(r1)))) #曲率が0となる距離。始終点の曲率が同符号の場合はL0<0 or L0>L、異符号の場合は0<L0<Lとなる。
             
             # クロソイドパラメータAの決定
@@ -132,7 +132,39 @@ class curve():
                 dist = np.linspace(-A**2/r1,-A**2/r2,n)
                 turn = -((L-L0)**2-L0**2)/(2*A**2) #緩和曲線通過前後での方位角変化
                 result=np.vstack((self.clothoid_dist(A,dist,'X'),self.clothoid_dist(A,dist,'Y')*(-1))).T
+        elif func == 'sin':
+            output = self.harfsin_intermediate(L, r1, r2, L)
+            tau1 = 0
+            turn = output[2]
+            rl = output[3] #if output[3] != 0 else np.inf
+            result = np.vstack((output[0],output[1])).T
+        else:
+            raise RuntimeError('invalid transition function')
+        
         return (np.dot(self.rotate(theta), np.dot(self.rotate(-tau1),(result-result[0]).T)).T)[1:], turn
+    def harfsin_intermediate(self, L, r1, r2, l_intermediate, dL=1):
+        def K(x,R1,R2,L):
+            '''sin半波長逓減の緩和曲線に対する曲率を返す。
+            x: 始点からの距離
+            R1: 始点での曲率半径
+            R2: 終点での曲率半径
+            L: 緩和曲線の全長
+            '''
+            return (1/R2-1/R1)/2*(np.sin(np.pi/L*x-np.pi/2)+1)+1/R1
+        if l_intermediate > 0:
+            if l_intermediate/5 <= dL:
+                dL = l_intermediate/5
+            tau_X = np.linspace(0,l_intermediate,int((l_intermediate)/dL)+1)
+            tau = integrate.cumtrapz(K(tau_X,r1,r2,L),tau_X,initial = 0)
+            X = integrate.cumtrapz(np.cos(tau),tau_X,initial = 0)
+            Y = integrate.cumtrapz(np.sin(tau),tau_X,initial = 0)
+            r_interm = 1/K(l_intermediate,r1,r2,L) if K(l_intermediate,r1,r2,L) != 0 else np.inf
+        else:
+            X = 0
+            Y = 0
+            tau = np.array([0])
+            r_interm = r1 if r1 != 0 else np.inf
+        return (X,Y,tau[-1],r_interm)
         
 class curve_intermediate(curve):
     def straight(self,L, theta, l_intermediate):
